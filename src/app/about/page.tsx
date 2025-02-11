df3f3fe75df4bf10b14716349f0a5050c9b39c5b
NicolasBonamy <n.bonamy@live.fr>
2025-02-11 12:43:04 +0100

    routing metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: "About testing metadata",
+};
+
 export default function About() {
   return <h1>About the project</h1>;
 }
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,8 +1,23 @@
-export default async function ProductDetails({
+import { Metadata } from "next";
+
+type Props = {
+  params: { productId: string };
+};
+
+export const generateMetadata = async ({
   params,
-}: {
-  params: Promise<{ productId: string }>;
-}) {
-  const productId = (await params).productId;
+}: Props): Promise<Metadata> => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`smartphone ${params.productId}`);
+    }, 100);
+  });
+  return {
+    title: `Product | ${title}`,
+  };
+};
+
+export default function ProductDetails({ params }: Props) {
+  const productId = params.productId;
   return <h1>Details about product {productId}</h1>;
 }
